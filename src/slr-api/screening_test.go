89f9d751bf0b8a9915dc9499ad30f7da4217add3
Wim Spaargaren <wim@weave.nl>
2020-06-17 21:52:28 +0200

    Lint issue

--- src/slr-api/screening_test.go
+++ src/slr-api/screening_test.go
@@ -181,8 +181,8 @@ func (s *ScreenTestSuite) TestOutputCreator() {
 			f1 = 1
 		}
 		predErr := float64(res.FalsePositive+res.FalseNegative) / float64(res.TruePositive+res.FalsePositive+res.TrueNegative+res.FalseNegative)
-
-		fmt.Println(fmt.Sprintf(`%d,"%f","%f","%f","%f","%f","%f","%f","%f","%f","%f"`, k, f1, precision, recall, predErr, res.Correct, res.Incorrect, res.FalseNegative, res.FalsePositive, res.TrueNegative, res.TruePositive))
+		fmt.Printf(`%d,"%f","%f","%f","%f","%f","%f","%f","%f","%f","%f"`, k, f1, precision, recall, predErr, res.Correct, res.Incorrect, res.FalseNegative, res.FalsePositive, res.TrueNegative, res.TruePositive)
+		fmt.Println()
 	}
 }
 
